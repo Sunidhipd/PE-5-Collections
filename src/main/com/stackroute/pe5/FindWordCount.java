c5727bf8fc5fc20670534361538eab74f62bf57e
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-14 11:59:00 +0530

    Update FindWordCount.java

--- src/main/com/stackroute/pe5/FindWordCount.java
+++ src/main/com/stackroute/pe5/FindWordCount.java
@@ -1,5 +1,10 @@
 package com.stackroute.pe5;
 
+/*Write a program to find the number of counts in the following String . Store the output in
+Map< String ,Integer> as key value pair.
+Input : String str = “one one -one___two,,three,one @three*one?two”;
+Output : {"one":5 , "two":2, "three" :2} */
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,25 +13,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FindWordCount {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the String: ");
-        String s = sc.nextLine();
-        Map<String, Integer> res = new HashMap<String, Integer>();
-        res = count(s);
-        Set<Map.Entry<String, Integer>> st = res.entrySet();
-
-        for (Map.Entry<String, Integer> me : st) {
-            System.out.print(me.getKey() + ": ");
-            System.out.println(me.getValue());
-        }
-    }
-
     public static Map<String, Integer> count(String s){
 
     Map<String, Integer> res = new HashMap<String, Integer>();
         String[] ar = s.split(" ");
-       // String regex = "";
 
         for (int i = 0; i < ar.length; i++) {
             Pattern p = Pattern.compile(ar[i]);
