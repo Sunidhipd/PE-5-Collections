39293a241123e919861c8372d66b1743a5acde36
Sunidhi Prasad <sunidhi15995@gmail.com>
2019-01-14 12:05:09 +0530

    Update KeyReplacement.java

--- src/main/com/stackroute/pe5/KeyReplacement.java
+++ src/main/com/stackroute/pe5/KeyReplacement.java
@@ -1,26 +1,18 @@
 package com.stackroute.pe5;
+/* Write a method that accepts a Map object having two key-value pairs with the keys val1 and val2.
+Modify and return the given map as follows:
+a. If the key `val1` has a value, set the key `val2` to have that value, and
+b. Set the key `val1` to have the value `" "` (empty string ).
+Example 1:
+The map {"val1": "java","val2": "c++"} should return {"val1": " ", "val2":"java"}
+Example 2:
+The map {"val1": "mars","val2": "saturn"}should return {"val1": " ", "val2":"mars"} */
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 public class KeyReplacement {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input 2 key value pair in the map: ");
-        Map<Object,String> map=new HashMap<Object, String>();
-        for (int i=0;i<2;i++) {
-            String s = sc.next();
-            String S=sc.next();
-            map.put(s,S);
-        }
-        Map<Object,String> res =replace(map);
-
-        for (Map.Entry<Object, String> pair: res.entrySet()) {
-            System.out.println(pair.getKey() + ":" + pair.getValue());
-        }
-    }
-
     public static Map<Object,String> replace(Map<Object,String> map){
         Object firstKey = map.keySet().toArray()[0];
         String valueForFirstKey = map.get(firstKey);
